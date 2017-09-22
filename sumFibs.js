4c1b190562105f6f617982d44f82758bcb0889c5
Alex Ticovschi <alexmticovschi@gmail.com>
2017-09-22 21:50:22 +0100

    Update sumFibs.js

--- sumFibs.js
+++ sumFibs.js
@@ -1,15 +1,14 @@
 
 function sumFibs(num) {
-
+  // Fibonacci numbers
   var fibs =  [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309];
   var odd = [];
   
-  fibs.map(function(item) {
-    if (item % 2 !== 0 && item <= num) {
-      odd.push(item);
-    }
-  });  
+  // loop through the fibs array; if the remainder of the fibonacci number is not equal to 0 
+  // ...AND the fibonacci number <= to input number, add it to odd array 
+  fibs.map(fib_num => (fib_num % 2 !== 0 && fib_num <= num) ? odd.push(fib_num) : false); 
   
+  // return the sum of all odd Fibonacci numbers 
   return odd.reduce((a,b) => a + b);
 }
 
