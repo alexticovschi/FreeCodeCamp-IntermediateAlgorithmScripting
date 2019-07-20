8decff713439bdeda2b4fd6ee49ed7442d6250f5
Alex Ticovschi <alexmticovschi@gmail.com>
2019-07-20 11:05:23 +0100

    second version solution

--- sumFibs.js
+++ sumFibs.js
@@ -1,4 +1,33 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that 
+// are less than or equal to num.
 
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional 
+// number in the sequence is the sum of the two previous numbers. The first six numbers 
+// of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less
+//  than or equal to 10 are 1, 1, 3, and 5
+
+/* Second version with filter and reduce */
+function sumFibs(num) {
+    const fibs = [1, 1];
+
+    for (let i = 1; i <= num; i++) {
+        fibs.push(fibs[i - 1] + fibs[i]);
+    }
+
+    return fibs.filter(n => {
+        if (n <= num) {
+            return n % 2 !== 0;
+        }
+    }).reduce((total, n) => total + n);
+}
+
+sumFibs(4);
+
+/#######################################################/ 
+
+/* First version */
 function sumFibs(num) {
   // Fibonacci numbers
   var fibs =  [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309];
