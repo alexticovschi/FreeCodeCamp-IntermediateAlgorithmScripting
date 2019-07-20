1c4c282e2cf5ab1d37130b1d896c066953142931
Alex Ticovschi <alexmticovschi@gmail.com>
2019-07-20 11:16:37 +0100

    third solution with loops

--- sumFibs.js
+++ sumFibs.js
@@ -8,7 +8,40 @@
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less
 //  than or equal to 10 are 1, 1, 3, and 5
 
-/* Second version with filter and reduce */
+
+
+/* Third solution with loops */
+
+function sumFibs(num) {
+  const fibs = [1,1];
+  let sumOdds = 0;
+
+  for (let i = 1; i <= num; i++) {
+    fibs.push(fibs[i-1] + fibs[i]);
+  }
+
+  // for (let i = 0; i <= fibs.length; i++) {    
+  //   if(fibs[i] <= num && fibs[i] % 2 !== 0) {
+  //       sumOdds += fibs[i];
+  //   }
+  // }
+
+  for (let n of fibs) {
+    if( (n <= num) && (n % 2 !== 0) ) {
+      sumOdds += n;
+    }
+  }
+  
+  return sumOdds;
+}
+
+sumFibs(4);
+
+
+/* ################################################################################################ */ 
+
+
+/* Second solution with filter and reduce */
 function sumFibs(num) {
     const fibs = [1, 1];
 
@@ -25,9 +58,11 @@ function sumFibs(num) {
 
 sumFibs(4);
 
-/#######################################################/ 
 
-/* First version */
+
+/* ################################################################################################ */ 
+
+/* First solution */
 function sumFibs(num) {
   // Fibonacci numbers
   var fibs =  [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309];
